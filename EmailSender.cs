3e65181d7cb3f927bfaa928c9a216fcdceb17e06
klmd <94202203+klmd@users.noreply.github.com>
2023-07-20 12:07:57 +0200

    Add files via upload

---
+++ EmailSender.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace DaPe.Utility;
+
+public class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        //logic to sent email
+        return Task.CompletedTask;
+    }
+}
